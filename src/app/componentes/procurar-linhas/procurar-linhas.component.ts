df05e49afa2abffc99bcecd08e74c4ff25ab81bd
Bruno Santos <bruno.santos@viaflow.net>
2019-05-16 09:33:31 -0300

    adionando lista para a busca

--- src/app/componentes/procurar-linhas/procurar-linhas.component.ts
+++ src/app/componentes/procurar-linhas/procurar-linhas.component.ts
@@ -4,12 +4,40 @@ import { LinhaOnibus } from 'src/app/model/linhaOnibus.model';
 import { Cliente } from 'src/app/model/cliente.model';
 import { ResponseApi } from 'src/app/model/responseApi.model';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-procurar-linhas',
   templateUrl: './procurar-linhas.component.html',
   styleUrls: ['./procurar-linhas.component.scss']
 })
 export class ProcurarLinhasComponent implements OnInit {
+
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   lat: number = -30.059434;
   lng: number = -51.173111;
